6ad835ed5092d1116a2c38b12d09eda85cb17474
GeeksforgeeksDS <courses@geeksforgeeks.org>
2024-09-25 14:09:30 +0530

    Add files via upload

---
+++ JBDL 71/JBDL-71-Major-main/JBDL-71-Major-main/TransactionService/src/main/java/org/gfg/transactionservice/constants/KafkaCostants.java
@@ -0,0 +1,10 @@
+package org.gfg.transactionservice.constants;
+
+public interface KafkaCostants {
+
+    String TRANSACTION_INITIATED_TOPIC ="transaction_initiated";
+
+    String TRANSACTION_UPDATED_TOPIC ="transaction_updated";
+
+//    String TRANSACTION_UPDATED_TOPIC = "TestMajor";
+}
